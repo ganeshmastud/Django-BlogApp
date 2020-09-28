404c5515ae2c2d315e35d387562f9188ba184b6c
GaneshMastud <mastudganesh6@gmail.com>
2020-09-28 11:47:00 +0530

    'change'

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4gxve*tz5ur&p7(6kos1o!=q91b3&fxc1b2i6#tqn05=bmhg8%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['blogapp1999.herokuapp.com']
+ALLOWED_HOSTS = ['blogapp1999.herokuapp.com','127.0.0.1']
 
 # Application definition
 
